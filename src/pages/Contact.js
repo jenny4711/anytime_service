259aec4aa0ed256fbc7d453da8dadf27f4a78b33
jiylee <jjenny4711@gmail.com>
2023-03-12 00:09:30 -0500

    ddd

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../Contact.css"
 
+
 const Contact = () => {
   return (
     <div className='Contact'>
--- src/pages/FeedBack.js
+++ src/pages/FeedBack.js
@@ -1,8 +1,65 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+import "../FeedBack.css"
 
 const FeedBack = () => {
+  const NewItemForm={
+    username:""
+    
+  }
+const [formData,setFormData]=useState(NewItemForm)
+const url='http://localhost:3000/feedback'
+const url1='https://jsonplaceholder.typicode.com/posts'
+
+  const handleChange=(e)=>{
+    const {name,value}=e.target;
+    setFormData(formData=>({
+      ...formData,
+      [name]:value
+    }))
+    console.log(formData)
+  }
+
+  const subMitHandler=(e)=>{
+    e.preventDefault()
+    console.log(formData)
+    console.log('test')
+    axios.post(url1,formData)
+    .then(response =>{
+      console.log(response)
+      
+    })
+   .catch(error=>{
+    console.log(error)
+   })
+
+  }
+
+
+
+  
+
+
+
+  
+
+  
   return (
-    <div>FeedBack</div>
+    <div className='FeedBack'>
+    <Form onSubmit={subMitHandler}>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>username</Form.Label>
+        <Form.Control name="username" value={formData.username} onChange={handleChange} type="text" placeholder="Username" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Feedback</Form.Label>
+        <Form.Control name="feedback" value={formData.feedback} onChange={handleChange} className='FeedBack-text' as="textarea" rows={3} />
+      </Form.Group>
+      <button>Submit</button>
+  
+    </Form>
+    </div>
   )
 }
 
