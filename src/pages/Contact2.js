ce229259dc8304437886498b28f52b7ed11ae45c
jiylee <jjenny4711@gmail.com>
2023-03-14 02:26:00 -0400

    fff

--- src/pages/Contact2.js
+++ src/pages/Contact2.js
@@ -6,13 +6,13 @@ const Contact = () => {
   return (
     <div className='Contact'>
     
-      <div className='Contact-info'>
-      <h2 className='Contact-title'>연락처</h2>
+      <ul className='Contact-info'>
+      <li className='Contact-li'>연락처</li>
       
-      <h3 className='Contact-phone'>전화번호:201-877-1844</h3>
-      <h3 className='Contact-email'>이메일:anysol.info@gmail.com</h3>
+      <li className='Contact-li'>전화번호:201-877-1844</li>
+      <li className='Contact-li'>이메일:anysol.info@gmail.com</li>
       
-      </div>
+      </ul>
    
       
     </div>
